babf41eaf523a44b79e382d2759d0ea6f15289f9
lucaspesso4 <dev.lucaspessoa@gmail.com>
2023-03-16 12:38:38 -0300

    feat (*): create Todo and User models

---
+++ TodoAPI/Models/TodoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoAPI.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Status { get; set; }
+    }
+}

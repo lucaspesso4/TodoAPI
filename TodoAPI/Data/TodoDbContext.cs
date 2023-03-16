15614fd27f302860177cf525659dae0f0f9656c5
lucaspesso4 <dev.lucaspessoa@gmail.com>
2023-03-16 13:34:22 -0300

    feat (*): create dbcontext

---
+++ TodoAPI/Data/TodoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using TodoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+            :base(options)
+        {   
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<TodoModel> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
